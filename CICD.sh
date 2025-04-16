29d9a4628234cbb6cd6866397cd65c88d656b598
Anthony Williams <mylifeassurance@aol.com>
2025-04-15 23:49:17 -0700

    Update CICD.sh
    Added severity for only critical outputs

--- CICD.sh
+++ CICD.sh
@@ -7,7 +7,8 @@ VERSION=INSECURE
 #BUILD: Clenaup old container/build image
 docker rm -f $APP_NAME 2>/dev/null || true
 docker build -t $APP_NAME:$VERSION . 
-docker scout cves mywebapp:INSECURE
+docker scout cves $APP_NAME:$VERSION --output ./vulns.report
+docker scout cves $APP_NAME:$VERSION --only-severity critical --exit-code
 
 #TEST: Run the container
 docker run -d -p 80:80 --name webapp $APP_NAME:$VERSION 
